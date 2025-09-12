7d7aad4ae28798a5db753722b807d60adb5d8b1e
Anthony Urrego <anthourrego@gmail.com>
2025-09-12 15:27:05 -0500

    Update Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'cs.prosof.TemparioApp',
   appName: 'TemparioApp',
-  webDir: 'www',
-  bundledWebRuntime: false
+  webDir: 'www'
 };
 
 export default config;
