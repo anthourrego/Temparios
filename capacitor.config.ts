32793a232e56038e6aed6d8d578fbe6ff2fed127
Anthony Urrego <anthourrego@gmail.com>
2025-09-12 16:18:04 -0500

    Migrate to capacitor 6

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,27 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'cs.prosof.TemparioApp',
   appName: 'TemparioApp',
-  webDir: 'www'
+  webDir: 'www',
+  android: {
+    allowMixedContent: true,
+    // Configuraciones adicionales para Android
+    webContentsDebuggingEnabled: true,
+    useLegacyBridge: false
+  },
+  server: {
+    // Permitir navegación a dominios específicos
+    /* allowNavigation: [
+      'http://192.168.0.224:8016',
+      'https://localhost',
+      'http://localhost',
+      'https://prosof.co:8011',
+      'http://prosof.co:8017'
+    ], */
+    cleartext: true,
+    // Configuración adicional para desarrollo
+    androidScheme: 'http', // Usar HTTP en desarrollo para evitar Mixed Content
+    iosScheme: 'ionic'
+  }
 };
 
 export default config;
