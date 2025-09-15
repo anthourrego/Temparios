585691afdcb12dfcdab17d3ccb0b1cb760280ab4
Anthony Urrego <anthourrego@gmail.com>
2025-09-15 15:58:53 -0500

    Update to version angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,32 +7,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CustomInjectorService } from './config/peticiones/peticion.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { Drivers } from '@ionic/storage';
 
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         AppComponent
     ],
-    imports: [
-        BrowserModule,
+    bootstrap: [AppComponent], imports: [BrowserModule,
         IonicModule.forRoot(),
         AppRoutingModule,
-        HttpClientModule,
         IonicStorageModule.forRoot({
             driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
             name: '__processDB',
             storeName: 'settings',
             description: 'TemparioApp data temp'
-        }),
-    ],
-    providers: [{
+        })], providers: [{
             provide: RouteReuseStrategy,
             useClass: IonicRouteStrategy
-        }],
-    bootstrap: [AppComponent]
-})
+        }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 	constructor(private injector: Injector) {
 		if (!CustomInjectorService.injector) {
